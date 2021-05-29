492e1c3c2ff5723b7a2ed91c642c7de03f863aed
MohammadFahad8 <fahadrao1718@gmail.com>
2021-05-29 10:57:37 +0500

    modal_box_head

--- public/scripttags/index.js
+++ public/scripttags/index.js
@@ -1,35 +1,28 @@
-console.log('Hello world!')
+function addHeadTag(tag, options) {
+    let item = document.createElement(tag);
+    Object.assign(item, options, { async: false });
+    document.querySelector("head").appendChild(item)
+  }
+  
+  function avoidFouc() {
+    document.body.style.opacity = 1;
+  }
+  addHeadTag('link', {
+    rel: 'stylesheet',
+    href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+    onload: avoidFouc
+  });
+  addHeadTag('script', {
+    type: 'application/javascript',
+    src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+  });
+  addHeadTag('script', {
+    type: 'application/javascript',
+    src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
+  });
+  addHeadTag('script', {
+    type: 'application/javascript',
+    src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+  });
 var div = document.getElementById("ScriptApp")
-div.innerHTML += '<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">'
-div.innerHTML += '<div class="modal-dialog modal-dialog-centered">'
-div.innerHTML += '<div class="modal-content">'
-div.innerHTML += '<div class="modal-header">'
-div.innerHTML += '<h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>'
-div.innerHTML += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
-div.innerHTML += ' </div>'
-div.innerHTML += '<div class="modal-body">'
-div.innerHTML += 'Show a second modal and hide this one with the button below.'
-div.innerHTML += '</div>'
-div.innerHTML += '<div class="modal-footer">'
-div.innerHTML += '<button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>'
-div.innerHTML += '</div>'
-div.innerHTML += '</div>'
-  div.innerHTML += '</div>'
-  div.innerHTML += '</div>'
-  div.innerHTML += '<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">'
-div.innerHTML += '<div class="modal-dialog modal-dialog-centered">'
-div.innerHTML += '<div class="modal-content">'
-div.innerHTML += '<div class="modal-header">'
-div.innerHTML += '<h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>'
-div.innerHTML += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
-div.innerHTML += '</div>'
-div.innerHTML += '<div class="modal-body">'
-div.innerHTML += 'Hide this modal and show the first with the button below.'
-div.innerHTML += '</div>'
-div.innerHTML += '<div class="modal-footer">'
-div.innerHTML += '<button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>'
-div.innerHTML += '</div>'
-div.innerHTML += '</div>'
-div.innerHTML += '</div>'
-div.innerHTML += '</div>'
-div.innerHTML += '<a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>'
+div.innerHTML += '<html><body><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button><div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Modal title</h5>        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>      </div><div class="modal-body">text</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div></body></html>'
