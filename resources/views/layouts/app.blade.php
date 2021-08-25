263b8040dc2149be9a8c74282d1fe21d000861ee
MohammadFahad8 <fahadrao1718@gmail.com>
2021-08-25 12:46:29 +0500

    sizecalculator_lazy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom-styling.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    
 </head>
 <body>
     <div id="app">
